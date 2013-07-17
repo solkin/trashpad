98a90cc5a2a7ec88895d63f6544c16f71e18d1cb
Solkin <inbox@tomclaw.com>
2013-07-17 19:14:03 +0400

    Update fetch_thread.php

--- fetch_thread.php
+++ fetch_thread.php
@@ -1,22 +1,3 @@
 <?php
-	$link = mysql_connect('localhost', 'root', '380349');
 
-	if (!$link) {
-		die('Connect error: ' . mysql_error());
-	}
-	echo 'OK';
-
-	$sql = "CREATE TABLE IF NOT EXISTS `threads` (
-		`id` int(16) unsigned NOT NULL auto_increment,
-		`name` text NOT NULL default '',
-		`feedback` text NOT NULL default '',
-		`geo` text NOT NULL default '',
-		`ip` text unsigned NOT NULL default '',
-		`message` text NOT NULL default '',
-		PRIMARY KEY  (`id`)
-		) ENGINE=MyISAM  DEFAULT CHARSET=utf8";
-
-	mysql_query($sql);
-
-	mysql_close($link);
 ?>
