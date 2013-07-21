b2ce2d701ded1aaaf4f8d68e8ebf9afdc76d95b4
Igor Solkin <inbox@tomclaw.com>
2013-07-22 00:55:16 +0400

    Modal window for new thread injection

--- index.php
+++ index.php
@@ -6,73 +6,105 @@
 		<link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
 		<link href="./bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 		<style type="text/css">
-		  body {
-			padding-top: 20px;
-			padding-bottom: 40px;
-		  }
+			body {
+				padding-top: 60px;
+				padding-bottom: 40px;
+			}
 
-		  /* Custom container */
-		  .container-narrow {
-			margin: 0 auto;
-			max-width: 1000px;
-		  }
-      </style>
+			/* Custom container */
+			.container-narrow {
+				margin: 0 auto;
+				max-width: 1000px;
+			}
+		</style>
 	</head>
 	<body>
-	<?php
-		include_once './service/connect_db.php';
-		include_once './service/utils.php';
-		include_once './service/settings.php';
 		
-		// Thread to show calculation.
-		$page_id = $_GET['page_id'];
-		
-		if($page_id == 0) {
-			$page_id = 1;
-		}
-		$thread_from = ($page_id - 1) * $threads_per_page;
-		
-		// Obtain required threads.
-		$threads_list = get_thread_list($link, true, $threads_per_page, $thread_from);
-		
-		echo '<div class="container-narrow">';
-		
-		// Page generation.
-		foreach($threads_list as $thread) {
-			$name = $thread['name'];
-			$feedback = $thread['feedback'];
-			$geo = $thread['geo'];
-			$thread_id = $thread['thread_id'];
-			$message = $thread['message'];
-			$reply_list = array_reverse($thread['reply']);
-			echo '<div class="row">';
-			echo '	<div class="span8">';
-			echo '	<p>'.$message.'</p>';
-			echo '	<div class="row">';
-			echo '		<div class="span7 offset1">';
-			echo '			<form class="form-inline" action="./service/post_reply.php" method="post">';
-			echo '				<input type="hidden" name="thread_id" value="'.$thread_id.'">';
-			echo '				<input type="text" name="message" placeholder="Your reply here">';
-			echo '				<button type="submit" class="btn">Reply</button>';
-			echo '			</form>';
-			echo '		</div>';
-			echo '	</div>';
-			foreach($reply_list as $reply) {
+		<div class="navbar navbar-fixed-top navbar-inverse" style="margin: -1px -1px 0;">
+		  <div class="navbar-inner">
+			<div class="container" style="width: auto; padding: 0 20px;">
+			  <a class="brand" href="#">Title</a>
+			  <ul class="nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#myModal" data-toggle="modal">Post</a></li>
+				<li><a href="#">Link</a></li>
+			  </ul>
+			</div>
+		  </div>
+		</div>
+		 
+		<!-- Modal -->
+		<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3 id="myModalLabel">Modal header</h3>
+		  </div>
+		  <div class="modal-body">
+			<p>One fine body…</p>
+		  </div>
+		  <div class="modal-footer">
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+			<button class="btn btn-primary">Save changes</button>
+		  </div>
+		</div>
+
+		<?php
+			include_once './service/connect_db.php';
+			include_once './service/utils.php';
+			include_once './service/settings.php';
+			
+			// Thread to show calculation.
+			$page_id = $_GET['page_id'];
+			
+			if($page_id == 0) {
+				$page_id = 1;
+			}
+			$thread_from = ($page_id - 1) * $threads_per_page;
+			
+			// Obtain required threads.
+			$threads_list = array_reverse(get_thread_list($link, true, $threads_per_page, $thread_from));
+			
+			echo '<div class="container-narrow">';
+			
+			// Page generation.
+			foreach($threads_list as $thread) {
+				$name = $thread['name'];
+				$feedback = $thread['feedback'];
+				$geo = $thread['geo'];
+				$thread_id = $thread['thread_id'];
+				$message = $thread['message'];
+				$reply_list = array_reverse($thread['reply']);
 				echo '<div class="row">';
-				echo '	<div class="span7 offset1">';
-				echo '	<p>'.$reply['message'].'</p>';
+				echo '	<div class="span8">';
+				echo '	<p>'.$message.'</p>';
+				echo '	<div class="row">';
+				echo '		<div class="span7 offset1">';
+				echo '			<form class="form-inline" action="./service/post_reply.php" method="post">';
+				echo '				<input type="hidden" name="thread_id" value="'.$thread_id.'">';
+				echo '				<input type="text" name="message" placeholder="Your reply here">';
+				echo '				<button type="submit" class="btn">Reply</button>';
+				echo '			</form>';
+				echo '		</div>';
+				echo '	</div>';
+				foreach($reply_list as $reply) {
+					echo '<div class="row">';
+					echo '	<div class="span7 offset1">';
+					echo '	<p>'.$reply['message'].'</p>';
+					echo '	</div>';
+					echo '</div>';
+				}
 				echo '	</div>';
 				echo '</div>';
 			}
-			echo '	</div>';
+			echo '<hr class="soften">';
+			echo '<div class="footer">';
+			echo '<p>&copy; TomClaw Software 2013</p>';
 			echo '</div>';
-		}
-		echo '<hr class="soften">';
-		echo '<div class="footer">';
-		echo '<p>&copy; TomClaw Software 2013</p>';
-		echo '</div>';
-      
-		echo '</div>';
-	?>
-  </body>
+		  
+			echo '</div>';
+		?>
+		
+		<script src="./bootstrap/js/jquery.js"></script>
+		<script src="./bootstrap/js/bootstrap-modal.js"></script>	
+	</body>
 </html>
--- service/thread_list.php
+++ service/thread_list.php
@@ -3,8 +3,13 @@
 	include_once './utils.php';
 
 	$include_reply = $_GET['include_reply'] == 'true' ? true : false;
+	$invert = $_GET['invert'] == 'true' ? true : false;
 	
 	$threads = get_thread_list($link, $include_reply);
+	
+	if($invert) {
+		$threads = array_reverse($threads);
+	}
 	
 	echo '{"threads": '.json_encode($threads).'}';
 ?>
