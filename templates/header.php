1cfd72fc6b299fcde5f9ce23f1c922880cc26f56
Solkin <inbox@tomclaw.com>
2013-09-15 12:47:20 +0400

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -17,7 +17,7 @@ function sanitize_output($buffer) {
 ob_start("sanitize_output");
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8"></meta>
