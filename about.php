bbf3988a9813d066c7aaf7b21ca5322a86304540
Solkin <inbox@tomclaw.com>
2013-09-20 20:33:40 +0400

    Базовая имплементация экрана "О сервисе".

--- about.php
+++ about.php
@@ -1,4 +1,28 @@
 <?php
 require_once './templates/header.php';
+?>
+<div class="jumbotron" style="margin: 0px; padding: 20px;">
+  <h2 style="margin-top: 0px;">TrashPad</h2>
+  <h3 style="margin-top: 0px;">Такого вам ещё не подавали.</h3>
+  <p class="lead">Максимум интерактива. Здесь и сейчас.</p>
+  <div class="row" style="text-align: center;">
+    <p style="margin-bottom: 0px;"><a class="btn btn-lg btn-success" href="#">Стоит попробовать</a></p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4">
+    <h2>Простота</h2>
+    <p>TrashPad сделан простым и понятным. Здесь нет сложных процедур регистрации, подтверждений, нет ограничений или лишний возможностей. Вы обязательно оцените это.</p>
+  </div>
+  <div class="col-lg-4">
+    <h2>Анонимность</h2>
+    <p>Вы когда-нибудь видели, чтобы блокнот требовал с вас документы? Каждый вправе оставить записку как простой прохожий. Свобода. Это даже важнее, чем кажется.</p>
+ </div>
+  <div class="col-lg-4">
+    <h2>Скорость</h2>
+    <p>Все страницы TrashPad оптимизированы и содержат только ту информацию, что нужна вам. А как он выглядит на современных смартфонах... </p>
+ </div>
+</div>
+<?php
 require_once './templates/footer.php';
 ?>
