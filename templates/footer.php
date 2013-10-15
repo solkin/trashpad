c0d5ca8ac965d19e7629660d5c7d0df7a7b15676
Solkin <inbox@tomclaw.com>
2013-10-16 01:51:17 +0400

    Добавлен счётчик метрики.

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,11 @@
 <hr class="soften"/>
-<div class="footer">
-  <p>&copy; TomClaw Software 2013</p>
+<div class="footer row">
+  <div class="col-lg-6">
+    <p>&copy; TomClaw Software 2013</p>
+  </div>
+  <div align="right" style="padding-right: 15px;">
+    <?php require_once 'counters.php'; ?>
+  </div>
 </div>
 </div>
 </body>
