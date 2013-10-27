9c923a69256122882925b4f95685d4438472e4c9
Solkin <inbox@tomclaw.com>
2013-10-27 15:04:57 +0400

    Небольшая правка вёрстки.

--- templates/burova.php
+++ templates/burova.php
@@ -1,6 +1,6 @@
-<div class="row" style="padding-bottom: 15px;" align="middle">
-  <img src="./images/system/burova.jpg" class="img-circle">
+<div style="padding-bottom: 15px; text-align: center;">
+  <div class="row" style="padding-bottom: 15px;">
+    <img src="./images/system/burova.jpg" class="img-circle" alt="Burova">
+  </div>
+  <p><?= _("Unfortunately, we have no threads here, but we have lots of others. Just press big green button!") ?></p>
 </div>
-<p align="center">
-  <?= _("Unfortunately, we have no threads here, but we have lots of others. Just press big green button!") ?>
-</p>
--- templates/marketing.php
+++ templates/marketing.php
@@ -3,12 +3,12 @@
     <img src="./images/system/burova_big.jpg" class="img-rounded img-responsive" alt="Burova">
   </div>
   <div class="row">
-  <div class="col-lg-3"></div>
-  <div class="col-lg-6">
-  <form class="form-inline" method="post">
-    <a href="#write_modal" data-toggle="modal" class="btn btn-lg btn-success btn-block"><?= _("Hello, world!") ?></a>
-  </form>
-  </div>
+    <div class="col-lg-3"></div>
+    <div class="col-lg-6">
+      <form class="form-inline" method="post">
+        <a href="#write_modal" data-toggle="modal" class="btn btn-lg btn-success btn-block"><?= _("Hello, world!") ?></a>
+      </form>
+    </div>
   </div>
   <div class="well" style="margin-top: 15px; margin-bottom: 0px">
     <p><?= _("TrashPad descriptions") ?></p><?= _("Away from the computer? Open TrashPad on the phone. It is fully optimized for smartphones.") ?>
@@ -24,5 +24,5 @@
   <div class="col-lg-4">
     <h3><?= _("Fast")?></h3>
     <p><?= _("Speed description")?></p>
-</div>
+  </div>
 </div>
