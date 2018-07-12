ff54d8f478f7f0b2f084794d191b84d99a291bea
Igor Solkin <inbox@tomclaw.com>
2018-07-12 11:36:24 +0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 </div>
 <hr class="soften"/>
 <div class="footer">
-  <p>&copy; TomClaw Software 2016</p>
+  <p>&copy; TomClaw Software 2018</p>
   <?php if(!$debug): ?>
   <?php require_once './include/counters.php'; ?>
   <?php endif; ?>
